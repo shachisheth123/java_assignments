35b87710e22b5e36085f509a785dfd643eb7e5f0
Sheth <shachi-sudesh.sheth@capgemini.com>
2019-03-12 17:45:02 +0530

    changes

--- EmployeeMain.java
+++ EmployeeMain.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-class EmployeeMain
+public class EmployeeMain
 {
 
 	public static void main(String[] args)
@@ -9,12 +9,12 @@ class EmployeeMain
 
          Employee employee1= new Employee(101,"Jhon",50000);		
 
-	 employee1.getAnnualBasic();
-	 employee1.getMonthlyGrossSalary();
-	 employee1.getAnnualGrossSalary();
-	 employee1.getMonthlyDeductions();
-	 employee1.getMonthlyTakeHome();
-	 employee1.getAnnualTakeHome();
+	 System.out.println("Annual basic salary is " +" " +employee1.getAnnualBasic());
+	 System.out.println("Monthly gross salary is" +" " +employee1.getMonthlyGrossSalary());
+	 System.out.println("Annual gross salary is" +" " +employee1.getAnnualGrossSalary());
+	 System.out.println("Monthly deductions are" +" " +employee1.getMonthlyDeductions());
+	 System.out.println("Monthly take home is" +" " +employee1.getMonthlyTakeHome());
+	 System.out.println("Annual tak home is " +" " +employee1.getAnnualTakeHome());
 	
 
 	}
