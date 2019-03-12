8d6a923d11667521ebf8a26bc079a18c32f484e8
Sheth <shachi-sudesh.sheth@capgemini.com>
2019-03-12 17:44:38 +0530

    changes

--- Employee.java
+++ Employee.java
@@ -5,11 +5,10 @@ class Employee
 	  private String employeeName;
 	  private double employeeMonthlyBasicSalary;
 
- 
 	public Employee(int employeeId , String employeeName , double employeeMonthlyBasicSalary)
 	{
 	this.employeeId=employeeId;
-	this.employeeNAme=employeeName;
+	this.employeeName=employeeName;
 	this.employeeMonthlyBasicSalary=employeeMonthlyBasicSalary;
 	}
 	
@@ -35,16 +34,47 @@ class Employee
 	public double getAnnualGrossSalary()
 	{
 		
-		return 12 * monthly_Gross;
+		return 12 * getMonthlyGrossSalary();
 		
 	}	
 	
 	public double getMonthlyDeductions()
 	{
 		
-		double pf_Amount = 0.10 * employeeMonthlyBasicSalary;
-		double esic_Amount= 4.75 * employeeMonthlyBasicSalary;
-		double prof_Tax;
+		double pf_Amount = 0.10 *  employeeMonthlyBasicSalary;
+		double esic_Amount= 4.75 *  employeeMonthlyBasicSalary;
+		double prof_Tax=100;
+
+		if(pf_Amount<6500 )
+		{
+			pf_Amount=pf_Amount;
+			esic_Amount=0;
+			if( employeeMonthlyBasicSalary==5000)
+			{
+				esic_Amount=esic_Amount;
+			}
+			else if(getMonthlyGrossSalary() <=10000)
+			{
+				prof_Tax=prof_Tax+50;
+			}
+
+			
+		}
+		else if(pf_Amount > 6500)
+		{
+			pf_Amount=6500;
+			esic_Amount=0;
+			if(getMonthlyGrossSalary()==5000)
+			{
+				esic_Amount=esic_Amount;
+			}
+			else if(getMonthlyGrossSalary() <=10000)
+			{
+				prof_Tax=prof_Tax+50;
+			}
+		}	
+	return pf_Amount+esic_Amount+prof_Tax;
+				
 		
 		
 	}
@@ -52,14 +82,14 @@ class Employee
 	public double getMonthlyTakeHome()
 	{
 		
-		return (monthly_Gross-monthly_Deduc);
+		return (getMonthlyGrossSalary()-getMonthlyDeductions());
 		
 	}
 
 
 	public double getAnnualTakeHome()
 	{
-		return 12*monthly_TakeHome;
+		return 12*getMonthlyTakeHome();
 	}
 
 
